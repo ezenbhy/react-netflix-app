d19333dac68e45292014479abbaf3d0f83b6e32d
ezenbhy <ezenbhy@gmail.com>
2022-10-18 17:35:36 +0900

    first commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -35,7 +35,7 @@ export default function Nav() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav__logo"
         //onClick={() => (window.location.href = "/")}
-        onClick={() => (navigate('/'))}
+        onClick={() => (history.pushState({pathname:"/"}))}
       />
 
       <input
